90c769579f997a922cf35e20918abfb910378f9e
Mahessh01 <Mahesshofficial@gmail.com>
2026-06-03 10:02:17 +0545

    some function added

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
 
-if (bar){
+if (bar) {
     bar.addEventListener('click', () => {
         nav.classList.add('active');
     });
 }
 
-
-const close = document.getElementById('close');
-if (close){
+if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active');
     });
@@ -19,23 +18,69 @@ const products = {
     1: {
         name: "Fashion Wear T-shirt",
         price: "$199",
-        image: "img/products/f1.jpg"
+        image: "img/products/f1.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae ligula nec magna varius tincidunt. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
     },
     2: {
-        name: "Light Pink Shirt",
+        name: "Astronaut T-Shirt",
         price: "$68",
-        image: "img/products/n1.jpg"
+        image: "img/products/f2.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae ligula nec magna varius tincidunt. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    },
+    3: {
+        name: "White Full Sleeve Shirt",
+        price: "$74",
+        image: "img/products/f3.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae ligula nec magna varius tincidunt. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    },
+    4: {
+        name: "Black Full Sleeve Shirt",
+        price: "$79",
+        image: "img/products/f4.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae ligula nec magna varius tincidunt. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    },
+    5: {
+        name: "Red Printed T-Shirt",
+        price: "$49",
+        image: "img/products/f5.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae ligula nec magna varius tincidunt. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    },
+    6: {
+        name: "Blue Printed T-Shirt",
+        price: "$49",
+        image: "img/products/f6.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae ligula nec magna varius tincidunt. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    },
+    7: {
+        name: "Green Printed T-Shirt",
+        price: "$49",
+        image: "img/products/f7.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae ligula nec magna varius tincidunt. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
+    },
+    8: {
+        name: "Yellow Printed T-Shirt",
+        price: "$49",
+        image: "img/products/f8.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae ligula nec magna varius tincidunt. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."
     }
 };
+
+//
+// ===== PRODUCT PAGE LOGIC =====
+// (Runs only if we are on sproduct.html)
+//
 
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 
-document.querySelector(".single-pro-details h4").innerText =
-    products[id].name;
+const title = document.querySelector(".single-pro-details h4");
+const price = document.querySelector(".single-pro-details h2");
+const desc = document.querySelector(".single-pro-details span");
+const mainImg = document.getElementById("MainImg");
 
-document.querySelector(".single-pro-details h2").innerText =
-    products[id].price;
-
-document.getElementById("MainImg").src =
-    products[id].image;
+if (title && price && mainImg && products[id]) {
+    title.innerText = products[id].name;
+    price.innerText = products[id].price;
+    desc.innerText = products[id].description;
+    mainImg.src = products[id].image;
+}
